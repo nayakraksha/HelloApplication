494fc0ff36997a2b52401b45d443cc9d42d4ec61
Raksha-Nayak <raksha_nayak@persistent.com>
2019-08-21 14:53:15 +0530

    added name

--- gittest/testing/src/calc.java
+++ gittest/testing/src/calc.java
@@ -2,7 +2,10 @@
 
 public class calc {
 	public static void main(String[] args) {
-		System.out.println("Welcome-home");
+
+        System.out.println("Welcome-home");
+        System.out.println("raksha");
+        
 	}
 	
  }
